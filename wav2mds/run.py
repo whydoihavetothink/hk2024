1e9bf21d3b5b87e1042955416220260c96db122f
SmarterFuture <anonymous.daisy.007@gmail.com>
2024-04-07 00:34:59 +0200

    minor changes

--- wav2mds/run.py
+++ wav2mds/run.py
@@ -11,7 +11,8 @@ def toMDS(file, sampleEnd = 100, threshhold = 0.37, numberOfNotes = 100):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         m = Main(file)
 
-        splitLengthinSeconds = 0.2
+        splitLengthinSeconds = 0.5
+        ms = int(splitLengthinSeconds * 1000)
         # multithreading
         song = m.split(splitLengthinSeconds, sampleBeginning = 0, sampleEnd = sampleEnd)
 
@@ -23,16 +24,20 @@ def toMDS(file, sampleEnd = 100, threshhold = 0.37, numberOfNotes = 100):
         final_notes = []
         winner = 0
         for note_list in notes:
-            count_dict = {i: note_list.count(i) for i in note_list}
-            if len(count_dict) == 0:
+            if not note_list:
                 final_notes.append(0)
                 continue
-            max_value = max(count_dict.values())
-            for k, v in count_dict.items():
-                if v == max_value:
-                    winner = k
-                    break
-            final_notes.append(round(440 * 2 ** (winner / 12), 2))
+            winner =note_list[0]
+            # count_dict = {i: note_list.count(i) for i in note_list}
+            # if len(count_dict) == 0:
+            #     final_notes.append(0)
+            #     continue
+            # max_value = max(count_dict.values())
+            # for k, v in count_dict.items():
+            #     if v == max_value:
+            #         winner = k
+            #         break
+            final_notes.append(round(440 * 2 ** (winner / 12)))
 
         final_notes_compiled = ""
 
@@ -44,23 +49,27 @@ def toMDS(file, sampleEnd = 100, threshhold = 0.37, numberOfNotes = 100):
                 new = False
                 counter += 1
             else:
-                final_notes_compiled += f"{ previous } { counter } ";
+                final_notes_compiled += f"{ previous } { counter * ms } "
                 counter = 1
                 new = True
             previous = note
         if new == True:
-            final_notes_compiled += f"{ previous } 1 "
+            final_notes_compiled += f"{ previous } { ms } "
         
         return final_notes_compiled
 
 
 if __name__ == "__main__":
     args = argv[1:]
+    print(args[0])
     file = AudioSegment.from_mp3(args[0])
     wav = "tmp.wav"
-    file.export(wav, fromat="wav")
+    file.export(wav, format="wav")
     board = serial.Serial(port="COM4", baudrate=115200, timeout=.1)
-    notes = toMDS(wav, sampleEnd = int(args[1]))
+    notes = toMDS(wav, sampleEnd = int(args[1])) + '\n'
+    print(notes)
 
-    board.write(bytes(notes, "utf8"))
+    board.write(bytes(notes, "ascii"))
+
+    print(board.readline())
 
